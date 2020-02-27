9c57d9325aea2214b7b1daf5d94b8d40b468bae0
Abril Vázquez <iabrilvzqz@gmail.com>
2020-02-27 07:11:53 -0600

    md2 funcionando correctamente para Shoology

---
+++ PracticalSession4/md2.py
@@ -0,0 +1,79 @@
+""" Author : Ilse Abril Vazquez Sanchez
+	Date : February 26, 2020 """
+
+import fileinput
+import binascii
+
+def padding(message):
+	# The message is increased to be a multiple of 16 bytes
+	i = 16 - (len(message) % 16)
+	# i bytes of value i are appended to the message
+	return message + bytearray(i for x in range(i))
+
+def checksum(paddedMessage, S):
+	# L and C (checksume) are initialized to 0's
+	C = bytearray([0 for x in range(16)])
+	L = 0
+
+	N = len(paddedMessage)
+
+	# Calculates the value of the checksum
+	for i in range(N // 16):
+		for j in range(16):
+			c = paddedMessage[ (16 * i) + j]
+			L = C[j] = C[j] ^ S[ c ^ L]
+
+	# The padded message is increased with the 16 bytes of the checksum
+	return paddedMessage + C 
+
+def hash(messagePlusChecksum , S):
+	# X is initialized to 0
+	X = bytearray([0 for x in range(48)])
+	N = len(messagePlusChecksum)
+
+
+	for i in range(N // 16):
+		for j in range(16):
+			X[ j + 16 ] = messagePlusChecksum[ (16 * i) + j]
+			X[ j + 32 ] = X[ j + 16 ] ^ X[j]
+
+		t = 0
+
+		for j in range(18):
+			for k in range(48):
+				t = X[ k ] ^ S[ t ]
+				X[ k ] = t
+
+			t = ( t + j ) % 256
+
+	# The first 16 bytes of X are the final hash
+	return X[:16]
+
+
+def md2():
+
+	# Sbox defined in the document
+	S = [41, 46, 67, 201, 162, 216, 124, 1, 61, 54, 84, 161, 236, 240, 6, 19, 98, 167, 5, 243, 192, 199, 115, 140, 152, 147, 43, 217, 188, 76, 130, 202, 30, 155, 87, 60, 253, 212, 224, 22, 103, 66, 111, 24, 138, 23, 229, 18, 190, 78, 196, 214, 218, 158, 222, 73, 160, 251, 245, 142, 187, 47, 238, 122, 169, 104, 121, 145, 21, 178, 7, 63, 148, 194, 16, 137, 11, 34, 95, 33, 128, 127, 93, 154, 90, 144, 50, 39, 53, 62, 204, 231, 191, 247, 151, 3, 255, 25, 48, 179, 72, 165, 181, 209, 215, 94, 146, 42, 172, 86, 170, 198, 79, 184, 56, 210, 150, 164, 125, 182, 118, 252, 107, 226, 156, 116, 4, 241, 69, 157, 112, 89, 100, 113, 135, 32, 134, 91, 207, 101, 230, 45, 168, 2, 27, 96, 37, 173, 174, 176, 185, 246, 28, 70, 97, 105, 52, 64, 126, 15, 85, 71, 163, 35, 221, 81, 175, 58, 195, 92, 249, 206, 186, 197, 234, 38, 44, 83, 13, 110, 133, 40, 132, 9, 211, 223, 205, 244, 65, 129, 77, 82, 106, 220, 55, 200, 108, 193, 171, 250, 36, 225, 123, 8, 12, 189, 177, 74, 120, 136, 149, 139, 227, 99, 232, 109, 233, 203, 213, 254, 59, 0, 29, 57, 242, 239, 183, 14, 102, 88, 208, 228, 166, 119, 114, 248, 235, 117, 75, 10, 49, 68, 80, 180, 143, 237, 31, 26, 219, 153, 141, 51, 159, 17, 131, 20]
+
+	# Reading file's content
+	lines = []
+	for line in fileinput.input():
+		lines.append(line.replace('\n', "").replace("\"\"", ""))
+
+	# Converts the string to its bytes representation
+	messageInBytes = bytearray(lines[0], 'utf-8')
+
+	paddedMessage = padding(messageInBytes)
+	
+	messagePlusChecksum = checksum(paddedMessage, S)
+
+	finalHash = hash(messagePlusChecksum, S)
+
+	# Returns the hexadecimal representation of the binary data
+	finalHash = binascii.hexlify(finalHash)
+
+	# Decodes the message digest
+	print(finalHash.decode('utf-8'))
+	
+md2()
+
