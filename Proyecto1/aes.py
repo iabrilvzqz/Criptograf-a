600f1a0bc5809af4c5af8fe4128c54b80bd3f33c
Abril Vázquez <iabrilvzqz@gmail.com>
2020-03-23 22:01:01 -0600

    AES y SHA con medición de tiempo

--- Proyecto1/aes.py
+++ Proyecto1/aes.py
@@ -1,45 +1,71 @@
-
-import os
+import time
+import csv
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
-from Crypto.Random import get_random_bytes
 
 def AES_ECB(key, data):
+	times = []
+
 	cipher = AES.new(key, AES.MODE_ECB)
 
+	start = time.clock()
 	ct = cipher.encrypt(pad(data, AES.block_size))
-	print(ct.hex())
+	end = time.clock()
 
+	print(ct.hex())
+	times.append(end-start)
+
+	start = time.clock()
 	pt = unpad(cipher.decrypt(ct), AES.block_size)
-	print('Pt', pt)
+	end = time.clock()
+
+	print(pt)
+	times.append(end-start)
+
+	return times
 
 
 def  AES_CBC(key, data):
-	
+
+	times = []
+
 	cipher = AES.new(key, AES.MODE_CBC)
 
+	start = time.clock()
 	ct = cipher.encrypt(pad(data, AES.block_size))
+	end = time.clock()
+
 	print(ct.hex())
+	times.append(end-start)
 
 	decipher = AES.new(key, AES.MODE_CBC, cipher.iv)
 
+	start = time.clock()
 	pt = unpad(decipher.decrypt(ct), AES.block_size)
-	print('Pt', pt)
+	end = time.clock()
 
+	print(pt)
+	times.append(end-start)
 
-key = b"80000000000000000000000000000000"
-data = b"abcdefghijklmnopqrstuvwxyz"
-print("AES ECB")
-AES_ECB(key, data)
-print("\nAES CBC")
-AES_CBC(key, data)
-"""
-aes_ec_code = "aes_ebc.encrypt(Padding.pad(inputBits, 16))"
-aes_dc_code = "aes_ebc.decrypt(ct)"
+	return times
 
-aes_ec_time = timeit.timeit(setup=aes_setup, stmt=aes_ec_code, number=100)
-aes_dc_time = timeit.timeit(setup=aes_setup, stmt=aes_dc_code, number=100)
+with open("messages.txt", 'r') as file, open("timesAES.csv", "w") as results:
+	
+	writer = csv.writer(results, quoting=csv.QUOTE_ALL)
+	writer.writerow(["AES-EBC E", "AES-EBC D", "AES-CBC E", "AES-CBC D"])
 
-print(aes_ec_time)
-print(aes_dc_time)"""
+	for line in file:
+
+		line = line.replace("\"\"", "").replace("\n", "")
+
+		data = bytes(line, "utf-8")		
+		key = bytes("0123456789ABCDEFGHIJKLMNOPQRSTUV", "utf-8")
+
+		print("\nAES ECB")
+		tebc = AES_ECB(key, data)
+
+		print("AES CBC")
+		tcbc = AES_CBC(key, data)
+
+		writer.writerow(tebc + tcbc)
--- Proyecto1/shas.py
+++ Proyecto1/shas.py
@@ -1,6 +1,13 @@
 import hashlib
+import time
+import csv
 
-with open("messages.txt", 'r') as file:
+with open("messages.txt", 'r') as file, open("timesSHA.csv", "w") as results:
+	
+	writer = csv.writer(results, quoting=csv.QUOTE_ALL)
+	writer.writerow(["SHA-1", "SHA-2", "SHA-3"])
+	
+	times = [0,0,0]
 
 	for line in file:
 
@@ -8,11 +15,25 @@ with open("messages.txt", 'r') as file:
 
 		message = line.encode()
 
+		start = time.clock()
 		SHA1 = hashlib.sha1(message).hexdigest()
-		SHA256 = hashlib.sha256(message).hexdigest()
-		SHA3 =  hashlib.sha3_256(message).hexdigest()
+		end = time.clock()
 
-		print("\nMensaje: " + line)
 		print("SHA1: " + SHA1)
+		times[0] = end-start
+
+		start = time.clock()
+		SHA256 = hashlib.sha256(message).hexdigest()
+		end = time.clock()
+
 		print("SHA2: "+ SHA256)
+		times[1] = end-start
+
+		start = time.clock()
+		SHA3 =  hashlib.sha3_256(message).hexdigest()
+		end = time.clock()
+
 		print("SHA3-256: " + SHA3)
+		times[2] = end-start
+
+		writer.writerow(times)
