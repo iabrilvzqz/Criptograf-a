50f34102450e77a56c4a0a60c6cb2042b903b6e2
Carlos Alberto <merecmoney3@gmail.com>
2020-03-24 21:28:53 -0600

    Keep public and private keys in memory to have better performance
     Keep public and private keys when encrypting, desencrypting, signing
     the data to improve performance. Add global variables to keep
     those values and just use them inside the function, which means
     code to read keys from files was removed

--- Proyecto1/RCA.py
+++ Proyecto1/RCA.py
@@ -7,7 +7,11 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto import Random
 
+publicKey = None
+privateKey = None
+
 def createRCAkeys(n, e, d):
+    global privateKey, publicKey
     # n = 0x9195E9854FA04A1433D4E22048951426A0ACFC6FE446730579D742CAEA5FDF6590FAEC7F71F3EBF0C6408564987D07E19EC07BC0F601B5E6ADB28D9AA6148FCC51CFF393178983790CC616C0EF34AB50DC8444F44E24117B46A47FA3630BF7E696865BFC245F7C3A314CD48C583D7B2223AF06881158557E37B3CC370AE6C8D5
     # e = 0x010001
     # d = 0x05B2DDE134ACB6E448E31C618720796EC9A5FBD0FAC3DC876A5832BFC94CD76C725B0AC6DCFF09F7F2CAB3C356F4B89F96F1E73B8BBAFABE7CD8C5BCE2A360BD8A3CE2767A2F83A6B143C2446D5A0388748F91813BB5E7A6CEA402368842DBC50C11EFE6B26CB08B53B83BC7FB17D5A62C39A6CCC718165D59375BE387642601
@@ -27,17 +31,12 @@ def createRCAkeys(n, e, d):
 def RSA_PSS(data):
     hashValue = SHA256.new(data)
 
-    privateKeyFile = open("./privateKey.pem", "r")
-    privateKey = RSA.importKey(privateKeyFile.read())
     signFunction = pss.new(privateKey)
     signature = signFunction.sign(hashValue)
 
     print(signature.hex())
 
-    publicKeyFile = open("./publicKey.pem", "r")
-    publicKey = RSA.importKey(publicKeyFile.read())
     verifier = pss.new(publicKey)
-
     try:
         verifier.verify(hashValue, signature)
         print("The signature is authentic.")
@@ -45,15 +44,11 @@ def RSA_PSS(data):
         print("The signature is not authentic.")
 
 def RSA_OAEP(data):
-    publicKeyFile = open("./publicKey.pem", "r")
-    publicKey = RSA.importKey(publicKeyFile.read())
     cipher = PKCS1_OAEP.new(publicKey)
 
     ciphertext = cipher.encrypt(data)
     print(ciphertext.hex())
 
-    privateKeyFile = open("./privateKey.pem", "r")
-    privateKey = RSA.importKey(privateKeyFile.read())
     decipher = PKCS1_OAEP.new(privateKey)
 
     plainText = decipher.decrypt(ciphertext)
@@ -68,6 +63,7 @@ with open("test.csv") as test_vectors:
         data =  bytes.fromhex(row[3])
         createRCAkeys(n, e, d)
         RSA_OAEP(data)
+        RSA_PSS(data)
 
 
 # createRCAkeys()
