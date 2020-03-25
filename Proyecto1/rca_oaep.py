6d0f481103de961c8c72355d753b49c1ba6b1060
Carlos Alberto <merecmoney3@gmail.com>
2020-03-25 00:42:41 -0600

    Add functional RCA OAEP
     - Read test vectors from rca_oaep_test_vectors.csv and create
     runnign time csv called times_RCA_OAEP.csv

---
+++ Proyecto1/rca_oaep.py
@@ -0,0 +1,48 @@
+import time
+import csv
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto import Random
+
+def RSA_OAEP(data, n, e, d):
+    times = []
+
+    privateKey = RSA.construct((n, e, d))
+    publicKey = privateKey.publickey()
+
+    cipher = PKCS1_OAEP.new(publicKey)
+
+    start = time.clock()
+    ciphertext = cipher.encrypt(data)
+    end = time.clock()
+    times.append(end - start)
+
+    print("Encrypted:")
+    print(ciphertext.hex())
+
+    decipher = PKCS1_OAEP.new(privateKey)
+
+    start = time.clock()
+    plainText = decipher.decrypt(ciphertext)
+    end = time.clock()
+    times.append(end - start)
+
+    print("Desencrypted:")
+    print(plainText.hex())
+
+    return times
+
+with open("rca_oaep_test_vectors.csv") as testVectors, open('times_RCA_OAEP.csv', 'w') as results:
+    reader = csv.reader(testVectors, delimiter = ",")
+    writer = csv.writer(results, quoting=csv.QUOTE_ALL)
+    writer.writerow(['DSA encrypting', 'DSA desencryting'])
+    for row in reader:
+        n = int(row[0], 16)
+        e = int(row[1], 16)
+        d = int(row[2], 16)
+        data =  bytes.fromhex(row[3])
+
+        print('\nRSA OAEP')
+        time_rsa_oaep = RSA_OAEP(data, n, e, d)
+
+        writer.writerow(time_rsa_oaep)
