fecb3b214a1ebaac61a9bb74bd917fbbfd8daada
Carlos Alberto <merecmoney3@gmail.com>
2020-03-24 21:38:20 -0600

    Change tabs to spaces

--- Proyecto1/dsa_ecdsa.py
+++ Proyecto1/dsa_ecdsa.py
@@ -5,73 +5,73 @@ from Cryptodome.Signature import DSS
 from Cryptodome.Hash import SHA512
 
 def DSA_1024(data):
-	times = []
+    times = []
 
-	key = DSA.generate(1024)
-	h = SHA512.new(data)
+    key = DSA.generate(1024)
+    h = SHA512.new(data)
 
-	controler = DSS.new(key, 'fips-186-3')
-	
-	start = time.clock()
-	signature = controler.sign(h)
-	end = time.clock()
+    controler = DSS.new(key, 'fips-186-3')
 
-	print(signature.hex())
-	times.append(end - start)
+    start = time.clock()
+    signature = controler.sign(h)
+    end = time.clock()
 
-	try:
-		start = time.clock()
-		controler.verify(h, signature)
-		end = time.clock()
-		print('The message is authentic')
-	except ValueError:
-		print('The message is not authentic')
-	
-	times.append(end - start)
+    print(signature.hex())
+    times.append(end - start)
 
-	return times
+    try:
+        start = time.clock()
+        controler.verify(h, signature)
+        end = time.clock()
+        print('The message is authentic')
+    except ValueError:
+        print('The message is not authentic')
+
+    times.append(end - start)
+
+    return times
 
 def ECDSA_521(data):
-	times = []
+    times = []
 
-	key = ECC.generate(curve = 'P-521')
-	h = SHA512.new(data)
+    key = ECC.generate(curve = 'P-521')
+    h = SHA512.new(data)
 
-	controler = DSS.new(key, 'fips-186-3')
+    controler = DSS.new(key, 'fips-186-3')
 
-	start = time.clock()
-	signature = controler.sign(h)
-	end = time.clock()
+    start = time.clock()
+    signature = controler.sign(h)
+    end = time.clock()
 
-	print(signature.hex())
-	times.append(end - start)
+    print(signature.hex())
+    times.append(end - start)
 
-	try:
-		start = time.clock()
-		controler.verify(h, signature)
-		end = time.clock()
-		print('The message is authentic')
-	except ValueError:
-		print('The message is not authentic')
-	
-	times.append(end - start)
+    try:
+        start = time.clock()
+        controler.verify(h, signature)
+        end = time.clock()
+        print('The message is authentic')
+    except ValueError:
+        print('The message is not authentic')
 
-	return times
+    times.append(end - start)
+
+    return times
 
 with open('messages2.txt', 'r') as file, open('timesDSA_ECDSA.csv', 'w') as results:
-	writer = csv.writer(results, quoting=csv.QUOTE_ALL)
-	writer.writerow(['DSA signing', 'DSA verifing', 'ECDSA signing', 'ECDSA verifing'])
+    writer = csv.writer(results, quoting=csv.QUOTE_ALL)
+    writer.writerow(['DSA signing', 'DSA verifing', 'ECDSA signing', 'ECDSA verifing'])
 
-	for line in file:
-		
-		line = line.replace('"','').replace('\n','')
-		
-		data = bytes.fromhex(line)
+    for line in file:
 
-		print('\nDSA')
-		time_dsa = DSA_1024(data)
+        line = line.replace('"','').replace('\n','')
 
-		print('\nECDSA')
-		time_ecdsa = ECDSA_521(data)
+        data = bytes.fromhex(line)
 
-		writer.writerow(time_dsa + time_ecdsa)
+        print('\nDSA')
+        time_dsa = DSA_1024(data)
+
+        print('\nECDSA')
+        time_ecdsa = ECDSA_521(data)
+
+        writer.writerow(time_dsa + time_ecdsa)
